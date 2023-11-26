78be0d3a3e8c33d29716adb2f35676ecf795fa64
tiffanyandjia <tlin37@buffalo.edu>
2023-11-25 22:00:55 -0500

    Navbar UI

--- airbnb/app/components/navbar/UserMenu.tsx
+++ airbnb/app/components/navbar/UserMenu.tsx
@@ -2,7 +2,14 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback } from "react";
+import Menuitem from "./MenuItem";
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -23,7 +30,7 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="
         p-4
         md:py-1
@@ -40,8 +47,33 @@ const UserMenu = () => {
         transition"
         >
           <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
         </div>
       </div>
+      {isOpen && (
+        <div
+          className="
+      absolute
+      rounded-xl
+      shadow-md
+      w-[40vw]
+      md:w-3/4
+      bg-white
+      overflow-hidden
+      right-0
+      top-12
+      text-sm"
+        >
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <Menuitem onClick={() => {}} label="Login" />
+              <Menuitem onClick={() => {}} label="Sign up" />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
