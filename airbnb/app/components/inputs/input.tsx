1555beaf2812d9e67de550e58640e3558dffa2e8
tiffanyandjia <tlin37@buffalo.edu>
2023-11-27 23:26:18 -0500

    structure

---
+++ airbnb/app/components/inputs/input.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { FieldValues, UseFormRegister } from "react-hook-form";
+import { BiDollar } from "react-icons/bi";
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled?: boolean;
+  formatPrice?: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+}
+
+const Input = () => {
+  return <div></div>;
+};
+
+export default Input;
--- airbnb/app/components/modals/RegisterModal.tsx
+++ airbnb/app/components/modals/RegisterModal.tsx
@@ -10,6 +10,8 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/input";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
