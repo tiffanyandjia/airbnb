9e2e239ee784713e5044ee045ff03619f0875540
tiffanydandjia <tlin37@buffalo.edu>
2023-12-03 15:13:18 -0500

    provider

--- airbnb/app/components/modals/RegisterModal.tsx
+++ airbnb/app/components/modals/RegisterModal.tsx
@@ -5,7 +5,7 @@ import { AiFillGithub } from "react-icons/ai";
 //import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
-//import {toast} from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
--- airbnb/app/layout.tsx
+++ airbnb/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/modals/Modal";
+import ToasterProvider from "./providers/ToasterProvider";
+import RegisterModal from "./components/modals/RegisterModal";
 
 export const metadata = {
   title: "Aribnb",
@@ -22,6 +24,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
           <Modal isOpen />
           <Navbar />
         </ClientOnly>
